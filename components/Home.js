d1c35ad879b6a2cc247ee2b5f7a6daf38ca1b2f5
NiazAhmed151 <niaz151@gmail.com>
2020-04-04 18:26:53 -0400

    Render FlatList with Pixaby Images

--- components/Home.js
+++ components/Home.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import {View, StyleSheet, TextInput } from 'react-native';
+import {View, StyleSheet, TextInput, Image, FlatList } from 'react-native';
 import {Icon, Button} from 'react-native-elements';
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 import {useSelector, useDispatch} from 'react-redux';
+import ImageList from './ImageList';
 
 function Home(){
 
+  // REDUX HOOKS
   const search_query = useSelector(state => state['search_query']);
+  const all_images = useSelector(state => state['image_data']['all_images'])
   const dispatch = useDispatch();
 
+  // API INFO
   const URL = 'https://pixabay.com/api/';
   const API_KEY = '15890560-8a70823bcdf077266eac5013e';
   
@@ -27,7 +31,12 @@ function Home(){
     })
     .then((data) => {
       var images = data['hits'].map( (item) => {
-        return item['previewURL']
+        return {
+          'id':item['id'], 
+          'height': item['previewHeight'],
+          'width': item['previewWidth'],
+          'url': item['previewURL']
+        }
       })
       dispatch({
         type:'UPDATE_ALL_IMAGES',
@@ -36,11 +45,19 @@ function Home(){
     });
   }
 
+  function renderImageList(){
+    console.log('i ran');
+    return all_images.length > 0 ? <ImageList/> : null ;
+  }
+
   return(
     <View style={styles.container}>
       <View style={styles.textInputContainer}>
         <TextInput style={styles.textArea} onChangeText={ text => onTextChange(text)} /> 
         <Button icon={<Icon name='search' size={15} color='white'/>} buttonStyle={styles.btnStyles} onPress={() => onSubmit()} color="#000"/>
+      </View>
+      <View style={styles.testContainer} >
+        <ImageList/>
       </View>
     </View>
   ) 
@@ -59,11 +76,24 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
   },
+  listContainer:{
+    height: 600,
+    width: wp("100%"),
+    borderWidth: 2,
+    borderColor:'black',
+    display:'flex',
+    alignItems:'center',
+    justifyContent:'center'
+  },  
   textArea: {
     height: hp("5%"),
     width: wp("68%"),
     borderBottomWidth: 1,
     borderColor:'black',
+  },
+  imgStyle: {
+    height: 20,
+    width: 20,
   },
   btnStyles:{
     backgroundColor: '#0B6AB0',
--- components/ImageList.js
+++ components/ImageList.js
@@ -1,20 +1,46 @@
 import React from 'react';
-import {FlatList, StyleSheet} from 'react-native';
+import {FlatList, StyleSheet, Image, TouchableOpacity} from 'react-native';
 import {useSelector} from 'react-redux';
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 
 function ImageList(){
 
-  const images = useSelector( state => state['image_data'])
+  const images = useSelector( state => state['image_data']['all_images']);
 
-  return(
-    <FlatList>
-
-    </FlatList>
-  )
-}
-
-const styles = StyleSheet.create({
-  list:{
+  function handleImgPress(){
     
   }
+
+  return( 
+    <FlatList
+      numColumns={2}
+      horizontal={false}
+      contentContainerStyle={styles.listStyles}
+      data = {images}
+      renderItem = { image =>  
+        <TouchableOpacity>
+          <Image 
+            style={styles.imgStyles} 
+            source={{uri:`${image['item']['url']}`}} 
+          />
+        </TouchableOpacity>} 
+      keyExtractor = { image => {return image['index']}}
+    />
+  ) 
+}
+
+const styles = StyleSheet.create({ 
+  listStyles:{
+   borderColor:'black',
+   display: 'flex',
+   alignItems: 'center',
+   justifyContent: 'space-between'
+  },  
+  imgStyles: {
+    width: 120,
+    height: 100,
+    margin: wp("5%")
+  }
 })
+
+export default ImageList;
