7b0e2513196df2311f2b4c6b73155acb793100b0
NiazAhmed151 <niaz151@gmail.com>
2020-04-04 21:08:01 -0400

    Extract Img Data from API

--- components/ImagePage.js
+++ components/ImagePage.js
@@ -1,20 +1,42 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, Image, StyleSheet} from 'react-native';
 import {useSelector} from 'react-redux';
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 
 function ImagePage({navigation}){
 
-  const single_img_data = useSelector(state => state['image_data']['single_image'])
-  console.log(single_img_data)
+  const single_img_data = useSelector(state => state['image_data']['single_image'][0]);
+  const img_url = single_img_data['largeImageURL'];
+  const img_likes = single_img_data['likes'];
+  const img_tags = single_img_data['tags'];
+  const img_downloads = single_img_data['downloads'];
+  
 
   return(
-    <View><Text> image page </Text></View>
+    <View style={styles.container} >
+      <Image source={{uri:img_url}} style={styles.imgStyle} />
+      <View style={styles.imgDetails}>
+
+      </View>
+    </View>
   )
 }
 
 const styles = StyleSheet.create({
   container:{
-
+    flex:1,
+    alignItems:'center',
+    justifyContent:'space-around'
+  },
+  imgStyle:{
+    height: hp("40%"),
+    width: wp("85%")
+  },
+  imgDetails:{
+    height: hp("40%"),
+    width: wp("85%"),
+    borderWidth: 2,
+    borderColor: 'black'
   }
 })
 
